d70de389b68a6f00a81cd53cf91edc16cb403e41
Aaron Maurice <Aaron@Aarons-iMac-at-Elvanto.local>
2015-01-22 14:24:53 +1000

    Initial

---
+++ login-wordpress.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: Elvanto Wordpress Login Widget
+    Plugin URI: https://www.elvanto.com
+    Description: Plugin for adding an Elvanto Login Widget to your Wordpress Site
+    Author: Aaron Maurice
+    Version: 1.0
+    Author URI: https://www.elvanto.com
+    */
+?>
